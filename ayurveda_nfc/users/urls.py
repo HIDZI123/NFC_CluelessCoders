975b769094236e9c45f9f77fd04611dbe273f47c
rya23 <aaryathakur48@gmail.com>
2023-09-15 16:48:48 +0530

    styled login

--- ayurveda_nfc/users/urls.py
+++ ayurveda_nfc/users/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("login",auth_views.LoginView.as_view(template_name="users/login.html"),name="login"),
     path("register", views.register, name="register"),
-
+    path("logout",auth_views.LogoutView.as_view(template_name="users/logout.html"),name="logout")
     
     
     ]
